8781eb3df71c7f06732daf7b472a83d4af9a2cc5
One E-sports <oneesports09@gmail.com>
2025-10-20 15:27:24 +0000

    Acca suno,  website background ei color hobe #D1DAE9   and  footer backg

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Footer() {
     return (
-        <footer className="bg-[#348234]/90 backdrop-blur-sm pb-20 md:pb-0 text-white">
+        <footer className="bg-[#1C2534]/90 backdrop-blur-sm pb-20 md:pb-0 text-white">
             <div className="container mx-auto px-4 py-8">
                 <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
                     <div>
