da0d5efde4d657bd9b306f3667fdf2d9662ae48d
One E-sports <oneesports09@gmail.com>
2025-10-20 16:28:58 +0000

    Try fixing this error: `Runtime Error: Error: CreditCardIcon is not defi

--- src/components/TopUpDetailClient.tsx
+++ src/components/TopUpDetailClient.tsx
@@ -14,6 +14,7 @@ import { useToast } from '@/hooks/use-toast';
 import { Minus, Plus, ShoppingCart, Zap, Gem, Info } from 'lucide-react';
 import { cn } from '@/lib/utils';
 import { Alert, AlertDescription } from './ui/alert';
+import { CreditCardIcon } from '@/components/icons';
 
 interface TopUpDetailClientProps {
   card: TopUpCardData;
