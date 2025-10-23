960c05aa043b339844b8be9a4ccc064ee1213072
One E-sports <oneesports09@gmail.com>
2025-10-23 09:57:56 +0000

    Favicon.ico ta chenge koro,,,  jeta logo deya ace seta set koro

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,9 @@ import AppLayout from '@/components/AppLayout';
 export const metadata: Metadata = {
   title: 'IHN TOPUP',
   description: 'Instant top-ups and digital cards.',
+  icons: {
+    icon: 'https://i.imgur.com/bJH9BH5.png',
+  },
 };
 
 export default function RootLayout({
